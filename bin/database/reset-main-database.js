c0becb16feb18e93af39a43e6515b7a526c40614
Jerrod Quintana <jerrodq2@yahoo.com>
2018-03-02 20:53:15 -0600

    cleaned up code and comments

--- bin/database/reset-main-database.js
+++ bin/database/reset-main-database.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//The below code assumes that the main database doesn't currently exists. Therefore, if we start by connecting to the default postgres empty database so we can make queries without erroring out
+//The below code assumes that the main database doesn't currently exists. Therefore, we start by connecting to the default empty postgres database so we can make queries without any errors
 require('dotenv').config();
 
 const connectionOptions = {
@@ -20,19 +20,17 @@ const knex = require('knex')(connectionOptions);
 console.log('New connection to default postgres database made');
 
 console.log('Terminating all other current connections to main database...');
+knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_DATABASE}'`)
 
-knex.raw(`select pg_terminate_backend(pid)
-from pg_stat_activity
-where datname = '${process.env.PG_DATABASE}'`)
 	.then(() => {
 		return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_DATABASE};`);
 	})
 	.then(() => {
 		console.log('Old database dropped if existed');
-		//create main database
+
 		return knex.raw(`CREATE DATABASE ${process.env.PG_DATABASE};`);
 	})
 	.then(() => {
-		console.log('Main database created!');
+		console.log('Main database created');
 		return process.exit();
 	});
