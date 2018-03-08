da53bfd3ef26a3c54628671ed9eaa737c702bd38
Jerrod Quintana <jerrodq2@yahoo.com>
2018-03-07 19:29:58 -0600

    updated the create-admin-and-fields file to include more fields and the created_at field. Slight change to eslint ignore file for the console.logs

--- bin/database/create-admin-and-fields.js
+++ bin/database/create-admin-and-fields.js
@@ -2,20 +2,66 @@
 
 
 const knex = require(`${process.cwd()}/src/config/knex`);
-const queries = [];
+
 
 const internals = {
 	createAdmin () {
-		return knex('admins').insert({ first_name: `${process.env.ADMIN_FIRST_NAME}`, last_name: `${process.env.ADMIN_LAST_NAME}`, username: `${process.env.ADMIN_USERNAME}`, email: `${process.env.ADMIN_EMAIL}` });
+		return knex('admins').insert({
+			first_name: `${process.env.ADMIN_FIRST_NAME}`,
+			last_name: `${process.env.ADMIN_LAST_NAME}`,
+			username: `${process.env.ADMIN_USERNAME}`,
+			email: `${process.env.ADMIN_EMAIL}`,
+			created_at: new Date() });
 	},
 
 	createFields () {
-		queries.push(
-			knex('fields').insert({ field: 'Web Development' })
-		);
-		queries.push(
-			knex('fields').insert({ field: 'Full Stack Web Development' })
-		);
+		const queries = [
+			knex('fields').insert({
+				field: 'Full Stack Web Development',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Full Stack Web Development',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Front End Web Development',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Back End Web Development',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Software Engineering',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Mobile App Development',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Web & Mobile Design',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Database Administration',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Ecommerce Development',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'QA & Testing',
+				created_at: new Date()
+			}),
+			knex('fields').insert({
+				field: 'Other',
+				created_at: new Date()
+			}),
+		];
+
 		return Promise.all(queries);
 	}
 };
@@ -27,4 +73,4 @@ Promise.all([
 	.then(() => {
 		console.log('Admin and inital fields created');
 		return process.exit();
-});
+	});
