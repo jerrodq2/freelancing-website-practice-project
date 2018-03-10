537af325a5db6be990634440ca751d85c164601f
Jerrod Quintana <jerrodq2@yahoo.com>
2018-03-10 14:44:11 -0600

    test env

--- bin/database/create-database.js
+++ bin/database/create-database.js
@@ -14,11 +14,11 @@ console.log('New connection to default postgres database made');
 
 
 console.log('Terminating all other current connections to main database...');
-
+console.log('!!!!!!!!!!!!', ` ${process.env.PG_DATABASE}`);
 knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_DATABASE}'`)
 	.then(() => {
 		console.log('Connections Terminated');
-		
+
 		return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_DATABASE};`);
 	})
 	.then(() => {
