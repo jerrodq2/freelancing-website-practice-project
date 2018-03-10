347f7e9da4743d59594c2d6a7e568c7d6d0fcd4b
Jerrod Quintana <jerrodq2@yahoo.com>
2018-03-09 21:13:36 -0600

    installed the code assertion library and the lab testing framework. Added the src folder to the test directory and added a simple test file to make sure that code, lab, and my test script work

---
+++ test/src/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const { expect } = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const { describe, it } = lab;
+
+describe('Checking that the testing', () => {
+
+	it('should work', () => {
+		expect(1).to.equal(1);
+	});
+});
