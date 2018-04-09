fd41265b102f2c982bcff84aeaa460bba1df3460
Jerrod Quintana <jerrodq2@yahoo.com>
2018-04-09 14:38:38 -0500

    finished the basic proposal model, added onto invitation model

--- src/models/proposals.js
+++ src/models/proposals.js
@@ -3,7 +3,7 @@
 
 const knex = require('../config/knex');
 const Model = require('./model');
-const Invitations = new Model('invitations');
+const Proposals = new Model('proposals');
 
 
 module.exports = {
@@ -13,31 +13,31 @@ module.exports = {
 	},
 	findOne (id) {
 		// specify the fields I want from each table
-		const invitationsColumns = ['invitations.*' ];
+		const proposalsColumns = ['proposals.*' ];
 		const jobColumns = ['j.title as job_title', 'j.rate as job_rate', 'j.rate_type as job_rate_type', 'j.description as job_description', 'j.experience_level_requested as job_experience_level_requested'];
 		const clientColumns = ['c.first_name as client_first_name', ' c.last_name as clients_last_name'];
 		const freelancerColumns = ['f.first_name as freelancer_first_name', 'f.last_name as freelancer_last_name', 'f.job_title as freelancer_job_title', 'f.experience_level as freelancer_experience_level'];
 
-		const selectedColumns = invitationsColumns.concat(jobColumns, clientColumns, freelancerColumns);
-		return knex('invitations')
+		const selectedColumns = proposalsColumns.concat(jobColumns, clientColumns, freelancerColumns);
+		return knex('proposals')
 			.select(selectedColumns)
-			.where(knex.raw(`invitations.id = '${id}'`))
-			.innerJoin('jobs as j', 'invitations.job_id', 'j.id')
-			.innerJoin('clients as c', 'invitations.client_id', 'c.id')
-			.innerJoin('freelancers as f', 'invitations.freelancer_id', 'f.id')
+			.where(knex.raw(`proposals.id = '${id}'`))
+			.innerJoin('jobs as j', 'proposals.job_id', 'j.id')
+			.innerJoin('clients as c', 'proposals.client_id', 'c.id')
+			.innerJoin('freelancers as f', 'proposals.freelancer_id', 'f.id')
 			.then((result) => result[0]);
 	},
 
 	create (data) {
-		return Invitations.create(data);
+		return Proposals.create(data);
 	},
 
 	update (id, data) {
-		return Invitations.updateById(id, data);
+		return Proposals.updateById(id, data);
 	},
 
 	delete (id) {
-		return Invitations.delete(id);
+		return Proposals.delete(id);
 	}
 
 };
