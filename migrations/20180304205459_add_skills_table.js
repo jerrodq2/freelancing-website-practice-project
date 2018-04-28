7cca1a9595d75c428a1f37c2ee2f554e6dc69037
Jerrod Quintana <jerrodq2@yahoo.com>
2018-04-28 14:22:29 -0500

    created the freelancer_skills table, altered the skills migration and seeds

--- migrations/20180304205459_add_skills_table.js
+++ migrations/20180304205459_add_skills_table.js
@@ -1,11 +1,10 @@
 'use strict';
 
 
-// Creates the skills table, used just with freelancers for their skills, ex: Python, HTML, Wordpress
+// Creates the skills table, used just with freelancers for their skills, ex: Python, HTML, Wordpress. Due to this relationship being a many to many, it goes through the table freelancer_skills, which acts as the middle man
 exports.up = (knex) => knex.schema.createTable('skills', (table) => {
 	table.uuid('id').notNullable().primary().defaultTo(knex.raw('gen_random_uuid()'));
 	table.string('skill').notNullable();
-	table.uuid('freelancer_id').notNullable().references('id').inTable('freelancers').onDelete('CASCADE');
 	table.timestamps();
 });
 
---
+++ migrations/20180428140127_add_freelancer_skills_table.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Creates the freelancer_skills table, used as a middle man between the many to many relationships of the freelancers and skills tables
+exports.up = (knex) => knex.schema.createTable('freelancer_skills', (table) => {
+	table.uuid('id').notNullable().primary().defaultTo(knex.raw('gen_random_uuid()'));
+	table.uuid('freelancer_id').notNullable().references('id').inTable('freelancers').onDelete('CASCADE');
+	table.uuid('skill_id').notNullable().references('id').inTable('skills').onDelete('CASCADE');
+	table.timestamps();
+});
+
+exports.down = (knex) => knex.schema.dropTable('freelancer_skills');
