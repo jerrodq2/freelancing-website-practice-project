b924dadf5ffa451d9f9a72f813c23804d3f28aaa
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-02 00:10:01 -0500

    added fields table

---
+++ src/models/fields.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+const Model = require('./model');
+const Fields = new Model('fields');
+
+
+module.exports = {
+
+	getAll () {
+		// determine if this is necessary
+	},
+
+	findOne (id) {
+		return Fields.findOne(id);
+	},
+
+	create (data) {
+		return Fields.create(data);
+	},
+
+	update (id, data) {
+		return Fields.updateById(id, data);
+	},
+
+	delete (id) {
+		return Fields.delete(id);
+	}
+
+};
