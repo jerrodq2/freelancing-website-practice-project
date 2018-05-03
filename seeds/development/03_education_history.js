dc6f7d46610d92a7d6cb3ab8c53d49fca42eb45f
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-03 10:49:41 -0500

    refactored the education and employment history to go through the models

--- seeds/development/03_education_history.js
+++ seeds/development/03_education_history.js
@@ -2,51 +2,48 @@
 
 
 const { format } = require('date-fns');
-const freelancers = require(`${process.cwd()}/seeds/ids/freelancers`);
+const freelancerIds = require(`${process.cwd()}/seeds/ids/freelancers`);
+const EducationHistory = require(`${process.cwd()}/src/models/education_history`);
 
-exports.seed = (knex) => {
+exports.seed = async (knex) => {
 	// Deletes ALL existing entries
-	return knex('education_history').del()
+	await knex('education_history').del();
+	// Inserts seed entries
+	return Promise.all([
+		EducationHistory.create({
+			degree: 'Bachelors',
+			school: 'Texas Tech University',
+			area_of_study: 'IT',
+			start_date: format(new Date(2011, 8, 1), 'YYYY-MM-DD'),
+			end_date: format(new Date(2015, 3, 1), 'YYYY-MM-DD'),
+			freelancer_id: freelancerIds.jerrod
+		}),
 
-		.then(function () {
-		// Inserts seed entries
-			return knex('education_history').insert([
-				{
-					degree: 'Bachelors',
-					school: 'Texas Tech University',
-					area_of_study: 'IT',
-					start_date: format(new Date(2011, 8, 1), 'YYYY-MM-DD'),
-					end_date: format(new Date(2015, 3, 1), 'YYYY-MM-DD'),
-					freelancer_id: freelancers.jerrod,
-					created_at: new Date()
-				},
-				{
-					degree: 'n/a',
-					school: 'Coding Dojo',
-					area_of_study: 'Full Stack Wev Development',
-					start_date: format(new Date(2016, 8, 1), 'YYYY-MM-DD'),
-					end_date: format(new Date(2016, 12, 24), 'YYYY-MM-DD'),
-					freelancer_id: freelancers.jerrod,
-					created_at: new Date()
-				},
-				{
-					degree: 'Masters',
-					school: 'Texas Tech University',
-					area_of_study: 'IT',
-					start_date: format(new Date(2011, 8, 1), 'YYYY-MM-DD'),
-					end_date: format(new Date(2015, 3, 1), 'YYYY-MM-DD'),
-					freelancer_id: freelancers.ryan,
-					created_at: new Date()
-				},
-				{
-					degree: 'Bachelors',
-					school: 'University Of North Texas',
-					area_of_study: 'IT',
-					start_date: format(new Date(2011, 8, 1), 'YYYY-MM-DD'),
-					end_date: format(new Date(2015, 3, 1), 'YYYY-MM-DD'),
-					freelancer_id: freelancers.jessica,
-					created_at: new Date()
-				}
-			]);
-		});
+		EducationHistory.create({
+			degree: 'n/a',
+			school: 'Coding Dojo',
+			area_of_study: 'Full Stack Wev Development',
+			start_date: format(new Date(2016, 8, 1), 'YYYY-MM-DD'),
+			end_date: format(new Date(2016, 12, 24), 'YYYY-MM-DD'),
+			freelancer_id: freelancerIds.jerrod
+		}),
+
+		EducationHistory.create({
+			degree: 'Masters',
+			school: 'Texas Tech University',
+			area_of_study: 'IT',
+			start_date: format(new Date(2011, 8, 1), 'YYYY-MM-DD'),
+			end_date: format(new Date(2015, 3, 1), 'YYYY-MM-DD'),
+			freelancer_id: freelancerIds.ryan
+		}),
+
+		EducationHistory.create({
+			degree: 'Bachelors',
+			school: 'University Of North Texas',
+			area_of_study: 'IT',
+			start_date: format(new Date(2011, 8, 1), 'YYYY-MM-DD'),
+			end_date: format(new Date(2015, 3, 1), 'YYYY-MM-DD'),
+			freelancer_id: freelancerIds.jessica
+		})
+	]);
 };
