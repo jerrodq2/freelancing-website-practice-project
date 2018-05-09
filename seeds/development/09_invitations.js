e1b0af037c7fc503f0556bded9edb0cad265e488
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-09 17:02:11 -0500

    added beginning of invitations seeds, changed 2 comments

--- seeds/development/09_invitations.js
+++ seeds/development/09_invitations.js
@@ -1,6 +1,33 @@
 'use strict';
 
 
-exports.seed = async (knex) => {
+const freelancerIds = require(`${process.cwd()}/seeds/ids/freelancers`);
+const clientIds = require(`${process.cwd()}/seeds/ids/clients`);
+const jobIds = require(`${process.cwd()}/seeds/ids/jobs`);
+const Invitations = require(`${process.cwd()}/src/models/invitations`);
 
+const today = new Date();
+// Create a date 2 weeks from now
+const futureDateOne = new Date();
+futureDateOne.setDate(today.getDate()+14);
+// Create a date 10 days from now
+const futureDateTwo = new Date();
+futureDateTwo.setDate(today.getDate()+10);
+
+exports.seed = async (knex) => {
+	// Deletes ALL existing entries
+	await knex('proposals').del();
+	// Inserts seed entries
+	return Promise.all([
+		Invitations.create({
+			title: 'Desperately need a wordpress developer',
+			description: 'Need some help, can you do it?',
+			status: 'accepted',
+			requested_time_limit: futureDateOne,
+			freelancer_id: freelancerIds.naruto,
+			client_id: clientIds.bruce,
+			job_id: jobIds.personal_website
+		}),
+
+	]);
 };
--- seeds/development/10_job_activity.js
+++ seeds/development/10_job_activity.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+// TODO: add a field to jobs to mark if they have been finished or not?
 exports.seed = async (knex) => {
 
 };
