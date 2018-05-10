0da272387d85bdab382b6b988a173d78cedbeda3
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-09 19:00:26 -0500

    added onto the remaining seeds slightly

--- seeds/development/13_saved_freelancers.js
+++ seeds/development/13_saved_freelancers.js
@@ -1,6 +1,14 @@
 'use strict';
 
 
+const freelancerIds = require(`${process.cwd()}/seeds/ids/freelancers`);
+const clientIds = require(`${process.cwd()}/seeds/ids/clients`);
+const jobIds = require(`${process.cwd()}/seeds/ids/jobs`);
+const SavedFreelancers = require(`${process.cwd()}/src/models/saved_freelancers`);
+
 exports.seed = async (knex) => {
+	// Deletes ALL existing entries
+	await knex('saved_freelancers').del();
+	// Inserts seed entries
 
 };
