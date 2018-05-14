11ea30ec27073cba884d2690940a7c8852979a6f
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-14 15:11:01 -0500

    testing circlie admins

--- test/src/models/admins.js
+++ test/src/models/admins.js
@@ -12,6 +12,15 @@ describe('Admins Model', () => {
 
 	describe('Has a findOne method', () => {
 		it('should retrieve a specific admin record', async() => {
+			await Admins.create({
+				id: 'ae5c05fd-f90b-4c00-b46d-c46bf397186a',
+				first_name: `${process.env.ADMIN_FIRST_NAME}`,
+				last_name: `${process.env.ADMIN_LAST_NAME}`,
+				username: `${process.env.ADMIN_USERNAME}`,
+				email: `${process.env.ADMIN_EMAIL}`,
+				password: `${process.env.ADMIN_PASSWORD}`
+			});
+
 			const admin = await Admins.findOne('ae5c05fd-f90b-4c00-b46d-c46bf397186a');
 
 			console.log('admin', admin);
