44e46048f15159886ff04cc0be0f3c12ed396a8a
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-19 20:22:05 -0500

    add test file for clients

---
+++ test/src/models/clients.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+const { expect } = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const { describe, it, before } = lab;
+const Clients = require(`${process.cwd()}/src/models/clients`);
+const { db, random, knex } = require(`${process.cwd()}/test/src/helpers`);
+
+
+describe('Clients Model', () => {
+
+	describe('has a create method', () => {
+
+	});
+
+
+	describe('has a findOne method', () => {
+
+	});
+
+
+	describe('has an update method', () => {
+
+	});
+
+
+	describe('has a delete method', () => {
+
+	});
+});
