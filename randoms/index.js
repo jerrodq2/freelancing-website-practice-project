e18c4e2c94f96945db3f846fe038cb07675f7d9c
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-19 23:11:40 -0500

    changed clients mixin to deal with the field_id now having to be given, to be tested

--- randoms/index.js
+++ randoms/index.js
@@ -9,7 +9,11 @@ random.mixin(require('./mixins'));
 
 // methods that create multiple records, ex: 10 clients, 20 skills, etc.
 random.mixin({
-	clients: (count = 10, opts = {}) => {
+	clients: async(count = 10, opts = {}) => {
+		if (!opts.field_id) {
+			opts.field_id = random.guid();
+			await random.field({ id: opts.field_id });
+		}
 		const clients = _.times(count, () => random.client(opts));
 		return Promise.all(clients);
 	},
