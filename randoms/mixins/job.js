e01b6926b73a82556a9487ef0f8d8eb4b4e3c036
Jerrod Quintana <jerrodq2@yahoo.com>
2018-05-23 16:03:39 -0500

    created job mixin

---
+++ randoms/mixins/job.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+const random = new (require('chance'));
+const Jobs = require(`${process.cwd()}/src/models/jobs`);
+
+// used to create a random job. If given no parameters, randomizes most fields.
+// A field_id and client_id is required, for simplicity we don't create them here
+module.exports = (opts = {}) => Jobs.create({
+	id: opts.id || random.guid(),
+	field_id: opts.field_id,
+	client_id: opts.client_id,
+	freelancer_id: opts.freelancer_id || null,
+	title: opts.title || random.sentence(),
+	rate: opts.rate || random.chance.integer({ min: 15, max: 80 }),
+	rate_type: opts.rate_type || 'hourly',
+	description: opts.description || random.paragraph(),
+	state: opts.state || 'TX',
+	city: opts.city || random.word(),
+	zip: opts.zip || random.zip(),
+	onsite_required: opts.onsite_required || random.bool(),
+	available: opts.available || random.bool(),
+	closed: opts.closed || random.bool(),
+	experience_level_requested: opts.experience_level_requested || 'any',
+});
--- test/src/models/jobs.js
+++ test/src/models/jobs.js
@@ -5,14 +5,40 @@ const { expect } = require('code');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const { describe, it, before } = lab;
-const Freelancers = require(`${process.cwd()}/src/models/freelancers`);
-const { db, random, knex } = require(`${process.cwd()}/test/src/helpers`);
+const Jobs = require(`${process.cwd()}/src/models/jobs`);
+const { db, random } = require(`${process.cwd()}/test/src/helpers`);
 
 
-describe('Jobs Model', () => {
+describe.only('Jobs Model', () => {
+	const id = random.guid(),
+		field_id = random.guid(),
+		client_id = random.guid(),
+		freelancer_id = random.guid(),
+		title = random.word(),
+		field = random.word(),
+		rate = 5000,
+		rate_type = 'flat',
+		description = random.paragraph(),
+		state = 'TX',
+		city = random.word(),
+		zip = random.zip(),
+		onsite_required = false,
+		available = true,
+		closed = false,
+		experience_level_requested = 'any',
+		data = { id, field_id, client_id, freelancer_id, title, rate, rate_type, description, state, city, zip, onsite_required, available, closed, experience_level_requested };
 
+	before(async() => {
+		await db.resetAll();
+		await random.field({ id: field_id, field });
+		await random.client({ id: client_id, field_id });
+		await random.freelancer({ id: freelancer_id, field_id });
+		return random.job(data);
+	});
 	describe('has a create method', () => {
-
+		it('should work...', async() => {
+			expect(1).to.equal(1);
+		});
 	});
 
 
