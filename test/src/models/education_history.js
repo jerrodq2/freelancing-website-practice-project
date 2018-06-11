c8bd9722bc815ff1a59961a6d8072072564fac8b
Jerrod Quintana <jerrodq2@yahoo.com>
2018-06-11 12:05:40 -0500

    created test file

---
+++ test/src/models/education_history.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+const { expect } = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const { describe, it, before } = lab;
+const EducationHistory = require(`${process.cwd()}/src/models/education_history`);
+const Freelancers = require(`${process.cwd()}/src/models/freelancers`);
+const { db, random, checkErr, _ } = require(`${process.cwd()}/test/src/helpers`);
+
+
+describe('Education History Model', () => {
+
+	describe('has a create method', () => {
+
+	});
+
+
+	describe('has a findHistory method', () => {
+
+	});
+
+
+	describe('has a findOne method', () => {
+
+	});
+
+
+	describe('has an update method', () => {
+
+	});
+
+
+	describe('has a delete method', () => {
+
+	});
+});
