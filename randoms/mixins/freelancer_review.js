6d03f809b53d49cfc4d7192031b899bc011db5bd
Jerrod Quintana <jerrodq2@yahoo.com>
2018-06-15 08:47:27 -0500

    changed all mixins to create their necessary foreign keys if not given

--- randoms/mixins/freelancer_review.js
+++ randoms/mixins/freelancer_review.js
@@ -5,11 +5,38 @@ const random = new (require('chance'));
 const FreelancerReviews = require(`${process.cwd()}/src/models/freelancer_reviews`);
 
 // used to create a random freelancer_review. If given no parameters, randomizes all fields. It requires the freelancer_id, client_id, and job_id to be given, for simplicity they're not created here.
-module.exports = (opts = {}) => FreelancerReviews.create({
-	id: opts.id || random.guid(),
-	rating: opts.rating || random.integer({ min: 1, max: 20 }),
-	review: opts.review || random.paragraph(),
-	freelancer_id: opts.freelancer_id,
-	client_id: opts.client_id,
-	job_id: opts.job_id,
-});
+module.exports = async(opts = {}) => {
+	// incase we need a field_id for the three below conditionals, we only have to create a field once
+	let field_id;
+	const createFieldId = async() => {
+		field_id = random.guid();
+		await random.field({ id: field_id });
+	};
+
+	// if the needed foreign keys aren't given, we create them here
+	if (!opts.freelancer_id) {
+		if (!field_id) createFieldId();
+		opts.freelancer_id = random.guid();
+		await random.freelancer({ id: opts.freelancer_id, field_id });
+	}
+	if (!opts.client_id) {
+		if (!field_id) createFieldId();
+		opts.client_id = random.guid();
+		await random.client({ id: opts.client_id, field_id });
+	}
+	if (!opts.job_id) {
+		if (!field_id) createFieldId();
+		opts.job_id = random.guid();
+		await random.job({ id: opts.job_id, field_id });
+	}
+
+
+	return FreelancerReviews.create({
+		id: opts.id || random.guid(),
+		rating: opts.rating || random.integer({ min: 1, max: 20 }),
+		review: opts.review || random.paragraph(),
+		freelancer_id: opts.freelancer_id,
+		client_id: opts.client_id,
+		job_id: opts.job_id,
+	});
+};
