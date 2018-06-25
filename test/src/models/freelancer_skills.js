a49ef5dc020c335e547c2e64e3fab4b213d5532d
Jerrod Quintana <jerrodq2@yahoo.com>
2018-06-25 17:21:55 -0500

    created the test file for the freelancer_skills model

---
+++ test/src/models/freelancer_skills.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+const { expect } = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const { describe, it, before } = lab;
+const Admins = require(`${process.cwd()}/src/models/admins`);
+const { db, random, knex, checkErr } = require(`${process.cwd()}/test/src/helpers`);
+
+
+describe('Freelancer Skills Model', () => {
+
+	describe('has a create method', () => {
+
+	});
+
+
+	describe('has a findOne method', () => {
+
+	});
+
+
+	describe('has a getAll method', () => {
+
+	});
+
+
+	describe('has an update method', () => {
+
+	});
+
+
+	describe('has a delete method', () => {
+
+	});
+
+
+	describe('has cascading delete on freelancer_id', () => {
+
+	});
+});
