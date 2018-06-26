b3d8b16488f8ee30123ad893046bb7971d7f2c21
Jerrod Quintana <jerrodq2@yahoo.com>
2018-06-26 09:40:16 -0500

    fixed broken mixin

--- randoms/mixins/freelancer_skill.js
+++ randoms/mixins/freelancer_skill.js
@@ -6,22 +6,22 @@ const FreelancerSkills = require(`${process.cwd()}/src/models/freelancer_skills`
 
 // used to create a random freelancer_skill. If given no parameters, randomizes all fields
 module.exports = async(opts = {}) => {
-	// if the needed foreign keys aren't given, we create them here
+	// if the needed keys aren't given, we create them here
 	if (!opts.freelancer_id) {
 		opts.freelancer_id = random.guid();
 		await random.freelancer({ id: opts.freelancer_id });
 	}
 
-	if (!opts.skill_id) {
-		opts.skill_id = random.guid();
-		await random.skill({ id: opts.skill_id });
+	// we give the name of a skill, and it is created if necessary in the model
+	if (!opts.skill) {
+		opts.skill = random.word();
+		await random.skill({ skill: opts.skill });
 	}
 
 
 	return FreelancerSkills.create({
 		id: opts.id || random.guid(),
 		freelancer_id: opts.freelancer_id,
-		skill_id: opts.skill_id,
-		skill_alias: opts.skill_alias || random.word(),
+		skill: opts.skill,
 	});
 };
