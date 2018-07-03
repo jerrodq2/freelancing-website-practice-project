9091081d668cc8bd4bfa32dbc0a4760096eb2b32
Jerrod Quintana <jerrodq2@yahoo.com>
2018-07-03 14:08:46 -0500

    created single mixin and started setting up test file

--- test/src/models/proposals.js
+++ test/src/models/proposals.js
@@ -12,11 +12,50 @@ const Clients = require(`${process.cwd()}/src/models/clients`);
 const { db, random, checkErr } = require(`${process.cwd()}/test/src/helpers`);
 
 
+describe.only('Proposals Model', () => {
+	// These variables are used to create the proposal record
+	const id = random.guid(),
+		freelancer_id = random.guid(),
+		job_id = random.guid(),
+		client_id = random.guid(),
+		title = random.word(),
+		description = random.paragraph(),
+		status = 'pending',
+		data = { id, freelancer_id, job_id, client_id, title, description, status };
 
-describe('Proposals Model', () => {
+	// these variables are used to create the freelancer, client, and job, used in below tests for accuracy
+	const field_id = random.guid(),
+		freelancer_first_name = random.name(),
+		freelancer_last_name = random.name(),
+		client_first_name = random.name(),
+		client_last_name = random.name(),
+		job_title = random.word(),
+		rate = random.integer({ min: 10, max: 100 }),
+		rate_type = 'hourly',
+		job_description = random.paragraph(),
+		experience_level_requested = 'expert',
+
+		freelancerData = { id: freelancer_id, field_id, first_name: freelancer_first_name, last_name: freelancer_last_name },
+
+		clientData = { id: client_id, field_id, first_name: client_first_name, last_name: client_last_name },
+
+		jobData = { id: job_id, title: job_title, rate, rate_type, description: job_description, experience_level_requested, client_id, freelancer_id };
+
+
+	before(async() => {
+		await db.resetAll();
+		await random.field({ id: field_id });
+		await random.freelancer(freelancerData);
+		await random.client(clientData);
+		await random.job(jobData);
+		await random.proposal(data);
+	});
+
 
 	describe('has a create method', () => {
+		it('text', async() => {
 
+		});
 	});
 
 
