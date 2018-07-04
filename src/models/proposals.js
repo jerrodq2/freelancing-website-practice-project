33b93799c97a0742cf4ce424d836064bac6b6209
Jerrod Quintana <jerrodq2@yahoo.com>
2018-07-04 13:44:21 -0500

    finished testing create method, added onto model to handle errors and proper checks

--- src/models/proposals.js
+++ src/models/proposals.js
@@ -4,13 +4,67 @@
 const knex = require('../config/knex');
 const Model = require('./main_model');
 const Proposals = new Model('proposals');
+const Errors = require(`${process.cwd()}/src/lib/errors`);
 
 
 module.exports = {
+	// TODO: make sure a job is still open before create, has to be available otherwise there's no point in the proposal. Send message like "job closed, no longer accepting proposals"
+	async create (data) {
+		const { job_id, freelancer_id } = data;
+
+		// first we make sure the job is still open/not closed
+		const check_job = await knex('jobs').where({ id: job_id })
+			.then((job) => {
+				// throw error if the job with the given id couldn't be found
+				if (!job[0]) throw Errors.badForeignKey('proposal', 'create', 'job_id');
+
+				return job[0];
+			})
+			.catch((err) => {
+				// throw error if the id wasn't given in proper uuid format
+				if (Errors.violatesIdSyntax(err))
+					throw Errors.badId('proposal', 'find');
+				// the cause of the error is most likely a missing job_id if this passes
+				if (!job_id)
+					throw Errors.badNull('proposal', 'create', 'job_id');
+				// if the cause of the error wasn't found above, throw the given error
+				throw err;
+			});
+		// if the job is closed, we raise an exception
+		if (check_job.closed) {
+			const message = 'The proposal you were trying to create can\'t be completed, this job is closed';
+
+			throw Errors.Boom.badRequest(message);
+		}
+
+		// next we check to see if this freelancer has already written a proposal for this job
+		const check_freelancer = await knex('proposals').where({ job_id, freelancer_id })
+			.catch((err) => {
+				// throw error if the id wasn't given in proper uuid format
+				if (Errors.violatesIdSyntax(err))
+					throw Errors.badId('proposal', 'find');
+				// the cause of the error is most likely a missing freelancer_id if this passes
+				if (!freelancer_id)
+					throw Errors.badNull('proposal', 'create', 'freelancer_id');
+				// if the cause of the error wasn't found above, throw the given error
+				throw err;
+			});
+
+		if (check_freelancer[0]) {
+			const message = 'The proposal you were trying to create can\'t be completed, this freelancer has already written a proposal for this job';
+
+			throw Errors.Boom.badRequest(message);
+		}
+
+		return Proposals.create(data);
+	},
+
 
 	getAll () {
 		// TODO: to be setup with pagination later
 	},
+
+
 	findOne (id) {
 		// specify the columns I want from each table
 		const proposalsColumns = ['proposals.*' ];
@@ -28,14 +82,11 @@ module.exports = {
 			.then((result) => result[0]);
 	},
 
-	// TODO: make sure a job is still open before create, has to be available otherwise there's no point in the proposal. Send message like "job closed, no longer accepting proposals"
-	create (data) {
-		return Proposals.create(data);
-	},
 
 	update (id, data) {
 		return Proposals.updateById(id, data);
 	},
+
 
 	delete (id) {
 		return Proposals.delete(id);
--- test/src/models/proposals.js
+++ test/src/models/proposals.js
@@ -15,7 +15,6 @@ const { db, random, checkErr } = require(`${process.cwd()}/test/src/helpers`);
 describe.only('Proposals Model', () => {
 	// simple function to round Javascript dates to the nearest date. When creating a new date with JS, it creates problems in the test, since the database rounds it to a day and saves it like that, so this eliminates that problem
 	const roundDate = (date) => {
-		console.log('hi?');
 		const offsetMs = date.getTimezoneOffset() * 60 * 1000, oneDayMs = 24 * 60 * 60 * 1000;
 		return new Date(Math.floor((date.getTime() - offsetMs) / oneDayMs) * oneDayMs + offsetMs);
 	};
@@ -53,7 +52,7 @@ describe.only('Proposals Model', () => {
 
 		clientData = { id: client_id, field_id, first_name: client_first_name, last_name: client_last_name },
 
-		jobData = { id: job_id, title: job_title, rate, rate_type, description: job_description, experience_level_requested, client_id, freelancer_id };
+		jobData = { id: job_id, title: job_title, rate, rate_type, description: job_description, experience_level_requested, client_id, freelancer_id, closed: 'false' };
 
 
 	before(async() => {
@@ -73,7 +72,7 @@ describe.only('Proposals Model', () => {
 			obj = { id: specificId, job_id: specificJobId },
 			createData = Object.assign({}, data, obj);
 
-		await random.job({ id: specificJobId, field_id, client_id, freelancer_id });
+		await random.job({ id: specificJobId, field_id, client_id, freelancer_id, closed: 'false' });
 
 		return createData;
 	};
@@ -105,7 +104,111 @@ describe.only('Proposals Model', () => {
 		});
 
 		it('should only allow you to create a new proposal if the job is still open and should raise an exception when you try to create it when the job is closed', async() => {
-			// TODO:
+			const specificId = random.guid(),
+				specificJobId = random.guid(),
+				obj = { id: specificId, job_id: specificJobId },
+				createData = Object.assign({}, data, obj);
+
+			await random.job({ id: specificJobId, field_id, client_id, freelancer_id, closed: true });
+
+			try {
+				await Proposals.create(createData);
+			} catch (err) {
+				expect(err).to.be.an.object();
+				const { message } = err;
+
+				expect(message).to.be.a.string();
+				expect(message).to.include('proposal');
+				expect(message).to.include('trying to create');
+				expect(message).to.include('can\'t be completed');
+				expect(message).to.include('this job is closed');
+			}
+		});
+
+		it('should only allow a freelancer to create one proposal per job and raise an exception on the second attempt', async() => {
+			const createData = await createNewData(),
+				specificId = createData.id,
+				specificJobId = createData.job_id,
+				secondId = random.guid(),
+				proposal = await Proposals.create(createData);
+
+			checkFields(proposal, specificId, specificJobId);
+
+			createData.id = secondId;
+			try {
+				await Proposals.create(createData);
+			} catch (err) {
+				expect(err).to.be.an.object();
+				const { message } = err;
+
+				expect(message).to.be.a.string();
+				expect(message).to.include('proposal');
+				expect(message).to.include('trying to create');
+				expect(message).to.include('can\'t be completed');
+				expect(message).to.include('freelancer has already written a proposal for this job');
+			}
+		});
+
+		it('should raise an exception if given an invalid id (not in uuid format', async() => {
+			const createData = await createNewData();
+			createData.id = 1;
+
+			return checkErr.checkIdFormat(Proposals, 'proposal', 'create', createData);
+		});
+
+		it('should require the freelancer_id to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(Proposals, 'proposal', createData, 'freelancer_id');
+		});
+
+		it('should require the client_id to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(Proposals, 'proposal', createData, 'client_id');
+		});
+
+		it('should require the job_id to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(Proposals, 'proposal', createData, 'job_id');
+		});
+
+		it('should require the title to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(Proposals, 'proposal', createData, 'title');
+		});
+
+		it('should require the description to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(Proposals, 'proposal', createData, 'description');
+		});
+
+		it('should require the status to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(Proposals, 'proposal', createData, 'status');
+		});
+
+
+		it('should raise an exception if given an incorrect freelancer_id (foreign key not found)', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkForeign(Proposals, 'proposal', 'create', createData, 'freelancer_id', random.guid());
+		});
+
+		it('should raise an exception if given an incorrect client_id (foreign key not found)', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkForeign(Proposals, 'proposal', 'create', createData, 'client_id', random.guid());
+		});
+
+		it('should raise an exception if given an incorrect job_id (foreign key not found)', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkForeign(Proposals, 'proposal', 'create', createData, 'job_id', random.guid());
 		});
 	});
 
