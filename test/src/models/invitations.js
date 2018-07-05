5fe988bbc6f32b8c22a8af41d13965a0b2fc3ef8
Jerrod Quintana <jerrodq2@yahoo.com>
2018-07-05 01:19:52 -0500

    created test file

---
+++ test/src/models/invitations.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+const { expect } = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const { describe, it, before } = lab;
+const Invitations = require(`${process.cwd()}/src/models/invitations`);
+const Jobs = require(`${process.cwd()}/src/models/jobs`);
+const Freelancers = require(`${process.cwd()}/src/models/freelancers`);
+const Clients = require(`${process.cwd()}/src/models/clients`);
+const { db, random, checkErr } = require(`${process.cwd()}/test/src/helpers`);
+
+
+describe('Invitations Model', () => {
+
+	describe('has a create method', () => {
+
+	});
+
+
+	describe('has a getAll method', () => {
+
+	});
+
+
+	describe('has a findOne method', () => {
+
+	});
+
+
+	describe('has an update method', () => {
+
+	});
+
+
+	describe('has a delete method', () => {
+
+	});
+
+
+	describe('has cascading delete on freelancer_id, client_id, and job_id', () => {
+
+	});
+});
