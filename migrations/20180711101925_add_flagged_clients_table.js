5258347743feeab26f6fc73dccccab17813020f7
Jerrod Quintana <jerrodq2@yahoo.com>
2018-07-11 10:31:00 -0500

    added todo comment

--- migrations/20180711101925_add_flagged_clients_table.js
+++ migrations/20180711101925_add_flagged_clients_table.js
@@ -1,8 +1,8 @@
-
+// TODO: o	Each table would need the respective id field (job_id), one field for being flagged by a client, and one for being flagged by a freelancer, and a field for the reason (message or enum?). The view would probably have a link to a pop up in order to create the flag, don't have to show that the item is flagged in the view (see upwork example)
 exports.up = function(knex, Promise) {
-  
+
 };
 
 exports.down = function(knex, Promise) {
-  
+
 };
