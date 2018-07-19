1f7ed70dc81798f887dd82fc34fb070deecdcaa2
Jerrod Quintana <jerrodq2@yahoo.com>
2018-07-19 14:39:24 -0500

    finished testing the create method, changes to the model accordingly

--- src/models/flagged_clients.js
+++ src/models/flagged_clients.js
@@ -3,12 +3,54 @@
 
 const Model = require('./main_model');
 const FlaggedClients = new Model('flagged_clients');
+const knex = require('../config/knex');
+const Errors = require(`${process.cwd()}/src/lib/errors`);
 
+
+const check_on_flag = async(data) => {
+	const result = await knex('flagged_clients').where(data)
+		.catch((err) => {
+			// throw error if the id wasn't given in proper uuid format
+			if (Errors.violatesIdSyntax(err))
+				throw Errors.badId('flagged_client', 'create');
+			// the cause of the error is most likely a missing client_id if this passes
+			if (!data.client_id)
+				throw Errors.badNull('flagged_client', 'create', 'client_id');
+			// if the cause of the error wasn't found above, throw the given error
+			throw err;
+		});
+
+	return result;
+};
 
 module.exports = {
-	// TODO: determine if we need two different methods for the flag being created by the client and by the freelancer. Perhaps it is only one create method, and which parameter (client_who_flagged/freelancer_who_flagged)is sent is determineded in the route
-	// TODO: Ensure a freelancer or client can only flag a specific job once (pre create check)
-	create (data) {
+	// TODO: determine which parameter (client_who_flagged/freelancer_who_flagged) is is sent in the route (maybe two different routes?)
+	async create (data) {
+		// first we grab the two potential types of users that created the flag and the client that is being flagged
+		const { client_who_flagged, freelancer_who_flagged, client_id } = data;
+		let createCheck, user;
+
+		// Now, we test which parameter was actually passed to us, and based on this, we can do a pre-create check to make sure the user hasn't already flagged this client
+		if (freelancer_who_flagged && !client_who_flagged) {
+			createCheck = await check_on_flag({ client_id, freelancer_who_flagged });
+			user = 'freelancer'; // used in the message below
+
+		} else if (!freelancer_who_flagged && client_who_flagged) {
+			createCheck = await check_on_flag({ client_id, client_who_flagged });
+			user = 'client'; // used in the message below
+
+		} else {
+			// if we reached this conditional, then we weren't passed either freelancer_who_flagged or client_who_flagged, so we raise an exception
+			throw Errors.badNull('flagged_client', 'create', 'freelancer_who_flagged and client_who_flagged'); //TODO: TEST THIS
+		}
+
+		// Next, we check to see if either of the above queries returned a result. If so, then this user has already flagged this client, so we raise an exception
+		if (createCheck[0]) {
+			const message = `The flagged_client you are trying to create can't be completed. This ${user} has already flagged this client`;
+
+			throw Errors.Boom.badRequest(message);
+		}
+
 		return FlaggedClients.create(data);
 	},
 
--- test/src/models/flagged_clients.js
+++ test/src/models/flagged_clients.js
@@ -56,13 +56,120 @@ describe.only('Flagged Clients Model', () => {
 	};
 
 	describe('has a create method', () => {
-		it('should create a new flagged_client if given valid data, create new created_at and updated_at fields, and return the new flagged_client object', async() => {
+		it('should allow you to create a new flagged_client if given valid data with the flag being created by a freelancer (freelancer_who_flagged), create new created_at and updated_at fields, and return the new flagged_client object', async() => {
 			const createData = await createNewData(),
 				specificId = createData.id,
 				specificClientId = createData.client_id,
 				flagged_client = await FlaggedClients.create(createData);
 
 			checkFields(flagged_client, specificId, specificClientId);
+		});
+
+		it('should allow you to create a new flagged_client if given valid data with the flag being created by a client (client_who_flagged), create new created_at and updated_at fields, and return the new flagged_client object', async() => {
+			const specificId = random.guid(),
+				// data = { id, client_id, freelancer_who_flagged, reason }
+				obj = { id: specificId, client_who_flagged, freelancer_who_flagged: null },
+				createData = Object.assign({}, data, obj),
+				flagged_client = await FlaggedClients.create(createData);
+
+			checkFields(flagged_client, specificId, client_id, client_who_flagged, null);
+		});
+
+		it('shouldn\'t allow a client to be flagged twice by the same freelancer and should raise an exception on the second attempt', async() => {
+			const createData = await createNewData(),
+				specificId = createData.id,
+				secondId = random.guid(),
+				specificClientId = createData.client_id,
+				flagged_client = await FlaggedClients.create(createData);
+
+			checkFields(flagged_client, specificId, specificClientId);
+			createData.id = secondId;
+
+			try {
+				await FlaggedClients.create(createData);
+			} catch (err) {
+				expect(err).to.be.an.object();
+				const { message } = err;
+				expect(message).to.be.a.string();
+				expect(message).to.include('flagged_client');
+				expect(message).to.include('create');
+				expect(message).to.include('can\'t be completed');
+				expect(message).to.include('freelancer');
+				expect(message).to.include('already flagged');
+			}
+		});
+
+		it('shouldn\'t allow a client to be flagged twice by the same client and should raise an exception on the second attempt', async() => {
+			const specificData = await createNewData(),
+				specificId = specificData.id,
+				secondId = random.guid(),
+				specificClientId = specificData.client_id,
+				obj = { client_who_flagged, freelancer_who_flagged: null },
+				createData = Object.assign({}, specificData, obj ),
+				flagged_client = await FlaggedClients.create(createData);
+
+			checkFields(flagged_client, specificId, specificClientId, client_who_flagged, null);
+			createData.id = secondId;
+
+			try {
+				await FlaggedClients.create(createData);
+			} catch (err) {
+				expect(err).to.be.an.object();
+				const { message } = err;
+				expect(message).to.be.a.string();
+				expect(message).to.include('flagged_client');
+				expect(message).to.include('create');
+				expect(message).to.include('can\'t be completed');
+				expect(message).to.include('client');
+				expect(message).to.include('already flagged');
+			}
+		});
+
+		it('should raise an exception if given an invalid id (not in uuid format', async() => {
+			const createData = await createNewData();
+			createData.id = 1;
+
+			return checkErr.checkIdFormat(FlaggedClients, 'flagged_client', 'create', createData);
+		});
+
+		it('should raise either freelancer_who_flagged or client_who_flagged and should raise an exception if given neither', async() => {
+			const createData = await createNewData();
+			createData.freelancer_who_flagged = null;
+
+			return checkErr.checkNotNull(FlaggedClients, 'flagged_client', createData, 'freelancer_who_flagged and client_who_flagged');
+		});
+
+		it('should require a client_id to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(FlaggedClients, 'flagged_client', createData, 'client_id');
+		});
+
+		it('should require a reason to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(FlaggedClients, 'flagged_client', createData, 'reason');
+		});
+
+
+		it('should raise an exception if given an incorrect client_id (foreign key not found)', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkForeign(FlaggedClients, 'flagged_client', 'create', createData, 'client_id', random.guid());
+		});
+
+		it('should raise an exception if given an incorrect freelancer_who_flagged (foreign key not found)', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkForeign(FlaggedClients, 'flagged_client', 'create', createData, 'freelancer_who_flagged', random.guid());
+		});
+
+		it('should raise an exception if given an incorrect client_who_flagged (foreign key not found)', async() => {
+			const createData = await createNewData();
+			createData.client_who_flagged = client_who_flagged;
+			createData.freelancer_who_flagged = null;
+
+			return checkErr.checkForeign(FlaggedClients, 'flagged_client', 'create', createData, 'client_who_flagged', random.guid());
 		});
 	});
 
