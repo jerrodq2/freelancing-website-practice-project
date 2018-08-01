fcabacada6ccafc8a24476948c34cd24b5727bf4
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-01 14:52:28 -0500

    finished testing the findOne method

--- test/src/models/flagged_client_reviews.js
+++ test/src/models/flagged_client_reviews.js
@@ -192,7 +192,55 @@ describe.only('Flagged Client Reviews Model', () => {
 
 
 	describe('has a findOne method', () => {
+		it('should retrieve a specific flagged_client_review record with a given id, and return the object with relevant information about the client_review that was flagged, the freelancer who flagged it and all info about the client_who_flagged equal to null if it was flagged by a freelancer', async() => {
+			const flagged_review = await FlaggedClientReviews.findOne(id);
 
+			expect(flagged_review).to.be.an.object();
+			expect(flagged_review.id).to.equal(id);
+			expect(flagged_review.client_review_id).to.equal(client_review_id);
+			expect(flagged_review.client_review_review_text).to.equal(review);
+			expect(flagged_review.client_who_flagged).to.equal(null);
+			expect(flagged_review.flagging_client_first_name).to.equal(null);
+			expect(flagged_review.flagging_client_last_name).to.equal(null);
+			expect(flagged_review.freelancer_who_flagged).to.equal(freelancer_who_flagged);
+			expect(flagged_review.flagging_freelancer_first_name).to.equal(freelancer_first_name);
+			expect(flagged_review.flagging_freelancer_last_name).to.equal(freelancer_last_name);
+			expect(flagged_review.reason).to.equal(reason);
+			expect(flagged_review.created_at).to.be.a.date();
+			expect(flagged_review.updated_at).to.equal(null);
+		});
+
+		it('should retrieve a specific flagged_client_review record with a given id, and return the object with relevant information about the client who flagged it and all info about the freelancer_who_flagged equal to null if it was flagged by a client', async() => {
+			const createData = await createNewData(),
+				specificId = createData.id,
+				specificReviewId = createData.client_review_id;
+			createData.freelancer_who_flagged = null;
+			createData.client_who_flagged = client_who_flagged;
+
+			await random.flagged_client_review(createData);
+			const flagged_review = await FlaggedClientReviews.findOne(specificId);
+
+			expect(flagged_review).to.be.an.object();
+			expect(flagged_review.id).to.equal(specificId);
+			expect(flagged_review.client_review_id).to.equal(specificReviewId);
+			expect(flagged_review.client_who_flagged).to.equal(client_who_flagged);
+			expect(flagged_review.flagging_client_first_name).to.equal(client_first_name);
+			expect(flagged_review.flagging_client_last_name).to.equal(client_last_name);
+			expect(flagged_review.freelancer_who_flagged).to.equal(null);
+			expect(flagged_review.flagging_freelancer_first_name).to.equal(null);
+			expect(flagged_review.flagging_freelancer_last_name).to.equal(null);
+			expect(flagged_review.reason).to.equal(reason);
+			expect(flagged_review.created_at).to.be.a.date();
+			expect(flagged_review.updated_at).to.equal(null);
+		});
+
+		it('should raise an exception if given an incorrect id (not found)', async() => {
+			return checkErr.checkNotFound(FlaggedClientReviews, 'flagged_client_review', 'find', random.guid());
+		});
+
+		it('should raise an exception when given an invalid id (not in uuid format)', async() => {
+			return checkErr.checkIdFormat(FlaggedClientReviews, 'flagged_client_review', 'find', {});
+		});
 	});
 
 
