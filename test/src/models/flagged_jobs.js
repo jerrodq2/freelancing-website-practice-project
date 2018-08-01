ab2e429b1b177162692bd962b3eb2d53a1a7a004
Jerrod Quintana <jerrodq2@yahoo.com>
2018-07-31 21:50:56 -0500

    finished testing create method, made changes to the model

--- test/src/models/flagged_jobs.js
+++ test/src/models/flagged_jobs.js
@@ -81,6 +81,112 @@ describe.only('Flagged Jobs Model', () => {
 
 			checkFields(flagged_job, specificId, specificJobId);
 		});
+
+		it('should allow you to create a new flagged_job if given valid data with the flag being created by a client (client_who_flagged), create new created_at and updated_at fields, and return the new flagged_job object', async() => {
+			const specificId = random.guid(),
+				obj = { id: specificId, client_who_flagged, freelancer_who_flagged: null },
+				createData = Object.assign({}, data, obj),
+				flagged_job = await FlaggedJobs.create(createData);
+
+			checkFields(flagged_job, specificId, job_id, client_who_flagged, null);
+		});
+
+		it('shouldn\'t allow a job to be flagged twice by the same freelancer and should raise an exception on the second attempt', async() => {
+			const createData = await createNewData(),
+				specificId = createData.id,
+				secondId = random.guid(),
+				specificJobId = createData.job_id,
+				flagged_job = await FlaggedJobs.create(createData);
+
+			checkFields(flagged_job, specificId, specificJobId);
+			createData.id = secondId;
+
+			try {
+				await FlaggedJobs.create(createData);
+			} catch (err) {
+				expect(err).to.be.an.object();
+				const { message } = err;
+				expect(message).to.be.a.string();
+				expect(message).to.include('flagged_job');
+				expect(message).to.include('create');
+				expect(message).to.include('can\'t be completed');
+				expect(message).to.include('freelancer');
+				expect(message).to.include('already flagged');
+			}
+		});
+
+		it('shouldn\'t allow a job to be flagged twice by the same client and should raise an exception on the second attempt', async() => {
+			const specificData = await createNewData(),
+				specificId = specificData.id,
+				secondId = random.guid(),
+				specificJobId = specificData.job_id,
+				obj = { client_who_flagged, freelancer_who_flagged: null },
+				createData = Object.assign({}, specificData, obj ),
+				flagged_job = await FlaggedJobs.create(createData);
+
+			checkFields(flagged_job, specificId, specificJobId, client_who_flagged, null);
+			createData.id = secondId;
+
+			try {
+				await FlaggedJobs.create(createData);
+			} catch (err) {
+				expect(err).to.be.an.object();
+				const { message } = err;
+				expect(message).to.be.a.string();
+				expect(message).to.include('flagged_job');
+				expect(message).to.include('create');
+				expect(message).to.include('can\'t be completed');
+				expect(message).to.include('client');
+				expect(message).to.include('already flagged');
+			}
+		});
+
+		it('should raise an exception if given an invalid id (not in uuid format', async() => {
+			const createData = await createNewData();
+			createData.id = 1;
+
+			return checkErr.checkIdFormat(FlaggedJobs, 'flagged_job', 'create', createData);
+		});
+
+		it('should require either freelancer_who_flagged or client_who_flagged and should raise an exception if given neither', async() => {
+			const createData = await createNewData();
+			createData.freelancer_who_flagged = null;
+
+			return checkErr.checkNotNull(FlaggedJobs, 'flagged_job', createData, 'freelancer_who_flagged and client_who_flagged');
+		});
+
+		it('should require a job_id to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(FlaggedJobs, 'flagged_job', createData, 'job_id');
+		});
+
+		it('should require a reason to create', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkNotNull(FlaggedJobs, 'flagged_job', createData, 'reason');
+		});
+
+
+		it('should raise an exception if given an incorrect job_id (foreign key not found)', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkForeign(FlaggedJobs, 'flagged_job', 'create', createData, 'job_id', random.guid());
+		});
+
+		it('should raise an exception if given an incorrect freelancer_who_flagged (foreign key not found)', async() => {
+			const createData = await createNewData();
+
+			return checkErr.checkForeign(FlaggedJobs, 'flagged_job', 'create', createData, 'freelancer_who_flagged', random.guid());
+		});
+
+		it('should raise an exception if given an incorrect client_who_flagged (foreign key not found)', async() => {
+			const createData = await createNewData();
+			createData.client_who_flagged = client_who_flagged;
+			createData.freelancer_who_flagged = null;
+
+			return checkErr.checkForeign(FlaggedJobs, 'flagged_job', 'create', createData, 'client_who_flagged', random.guid());
+		});
 	});
 
 
