2ba607f009d9f93847050257f45adee122557df4
Jerrod Quintana <jerrodq2@yahoo.com>
2018-07-31 22:09:16 -0500

    finished testing findOne method

--- test/src/models/flagged_jobs.js
+++ test/src/models/flagged_jobs.js
@@ -191,7 +191,56 @@ describe.only('Flagged Jobs Model', () => {
 
 
 	describe('has a findOne method', () => {
+		it('should retrieve a specific flagged_job record with a given id, and return the object with relevant information about the job that was flagged, the freelancer who flagged it and all info about the client_who_flagged equal to null if it was flagged by a freelancer', async() => {
+			const flagged_job = await FlaggedJobs.findOne(id);
 
+			expect(flagged_job).to.be.an.object();
+			expect(flagged_job.id).to.equal(id);
+			expect(flagged_job.job_id).to.equal(job_id);
+			expect(flagged_job.job_title).to.equal(title);
+			expect(flagged_job.job_description).to.equal(description);
+			expect(flagged_job.client_who_flagged).to.equal(null);
+			expect(flagged_job.flagging_client_first_name).to.equal(null);
+			expect(flagged_job.flagging_client_last_name).to.equal(null);
+			expect(flagged_job.freelancer_who_flagged).to.equal(freelancer_who_flagged);
+			expect(flagged_job.flagging_freelancer_first_name).to.equal(freelancer_first_name);
+			expect(flagged_job.flagging_freelancer_last_name).to.equal(freelancer_last_name);
+			expect(flagged_job.reason).to.equal(reason);
+			expect(flagged_job.created_at).to.be.a.date();
+			expect(flagged_job.updated_at).to.equal(null);
+		});
+
+		it('should retrieve a specific flagged_job record with a given id, and return the object with relevant information about the client who flagged it and all info about the freelancer_who_flagged equal to null if it was flagged by a client', async() => {
+			const createData = await createNewData(),
+				specificId = createData.id,
+				specificJobId = createData.job_id;
+			createData.freelancer_who_flagged = null;
+			createData.client_who_flagged = client_who_flagged;
+
+			await random.flagged_job(createData);
+			const flagged_job = await FlaggedJobs.findOne(specificId);
+
+			expect(flagged_job).to.be.an.object();
+			expect(flagged_job.id).to.equal(specificId);
+			expect(flagged_job.job_id).to.equal(specificJobId);
+			expect(flagged_job.client_who_flagged).to.equal(client_who_flagged);
+			expect(flagged_job.flagging_client_first_name).to.equal(client_first_name);
+			expect(flagged_job.flagging_client_last_name).to.equal(client_last_name);
+			expect(flagged_job.freelancer_who_flagged).to.equal(null);
+			expect(flagged_job.flagging_freelancer_first_name).to.equal(null);
+			expect(flagged_job.flagging_freelancer_last_name).to.equal(null);
+			expect(flagged_job.reason).to.equal(reason);
+			expect(flagged_job.created_at).to.be.a.date();
+			expect(flagged_job.updated_at).to.equal(null);
+		});
+
+		it('should raise an exception if given an incorrect id (not found)', async() => {
+			return checkErr.checkNotFound(FlaggedJobs, 'flagged_job', 'find', random.guid());
+		});
+
+		it('should raise an exception when given an invalid id (not in uuid format)', async() => {
+			return checkErr.checkIdFormat(FlaggedJobs, 'flagged_job', 'find', {});
+		});
 	});
 
 
