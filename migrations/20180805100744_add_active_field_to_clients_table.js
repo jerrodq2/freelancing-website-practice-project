81e071d137d54252f22eb7b56dbfbb388798217e
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-06 16:27:45 -0500

    added comments to migration files

--- migrations/20180805100744_add_active_field_to_clients_table.js
+++ migrations/20180805100744_add_active_field_to_clients_table.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+// Adds an active field to the clients table. This is a boolean that takes the place of delete, allowing clients to be marked as inactive, thus not showing up in any searches, or allowing them to create any jobs, while also still remaining in the database and not having to be deleted
 exports.up = (knex) => {
 	return knex.schema.alterTable('clients', (table) => table.boolean('active').notNullable().defaultTo(true));
 };
--- migrations/20180805100759_add_active_field_to_freelancers_table.js
+++ migrations/20180805100759_add_active_field_to_freelancers_table.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+// Adds an active field to the freelancers table. This is a boolean that takes the place of delete, allowing freelancers to be marked as inactive, thus not showing up in any searches, or allowing them to take any jobs, while also still remaining in the database and not having to be deleted
 exports.up = (knex) => {
 	return knex.schema.alterTable('freelancers', (table) => table.boolean('active').notNullable().defaultTo(true));
 };
