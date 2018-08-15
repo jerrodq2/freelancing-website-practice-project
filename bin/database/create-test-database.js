5d270916a7d4dab7136b64b8b8d5b8a9cd570755
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-15 12:32:48 -0500

    testing out moving the db setup code to a function

--- bin/database/create-test-database.js
+++ bin/database/create-test-database.js
@@ -9,23 +9,24 @@ const connectionOptions = require(`${process.cwd()}/knexfile.js`)[`${process.env
 connectionOptions.connection.database = 'postgres';
 
 
-const knex = require('knex')(connectionOptions);
-console.log('New connection to default postgres database made');
+// I put the setting up database code into a function so I can control all of the async code, including the require statement which seemed to give me some trouble
+const SetUpTestDB = async () => {
+	const knex = await require('knex')(connectionOptions);
+	console.log('New connection to default postgres database made');
+
+	// Remove all other connections to test database
+	await knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`);
+	console.log('Removed all other connections to the test database');
+
+	// Drop test database if it exists
+	await knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
+	console.log('Dropped test database (if it existed)');
+
+	// Create test database
+	await knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
+	console.log('Test database created');
+};
 
 
-// Remove all other connections to test database
-knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`)
-	.then(() => {
-		console.log('Removed all other connections to the test database');
-		// Drop test database if it exists
-		return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
-	})
-	.then(() => {
-		console.log('Dropped test database (if it existed)');
-		// Create test database
-		return knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
-	})
-	.then(() => {
-		console.log('Test database created');
-		return process.exit();
-	});
+SetUpTestDB()
+	.then(() => process.exit());
