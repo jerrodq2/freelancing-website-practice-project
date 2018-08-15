abe953dfa7ed0277951ee7550b0eb888761a45b2
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-15 12:51:02 -0500

    added catch

--- bin/database/create-test-database.js
+++ bin/database/create-test-database.js
@@ -15,7 +15,12 @@ const SetUpTestDB = async () => {
 	console.log('New connection to default postgres database made');
 
 	// Remove all other connections to test database
-	await knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`);
+	await knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`)
+		.catch((err) => {
+			console.log('there was an error!!!!!!!!!!!');
+			console.log(err);
+			return;
+		})
 	console.log('Removed all other connections to the test database');
 
 	// Drop test database if it exists
