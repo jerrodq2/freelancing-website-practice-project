c352c5e1305ee6c19edca40aca3ca915d006e551
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-15 13:44:20 -0500

    testing going back and taking a piece of code out

--- bin/database/create-test-database.js
+++ bin/database/create-test-database.js
@@ -5,36 +5,19 @@
 require('dotenv').config();
 
 const connectionOptions = require(`${process.cwd()}/knexfile.js`)[`${process.env.NODE_ENV}`];
-// Change database in connection to default 'postgres' db
 connectionOptions.connection.database = 'postgres';
 
-
-// I put the setting up database code into a function so I can control all of the async code, including the require statement which seemed to give me some trouble
-const SetUpTestDB = async () => {
-	const knex = await require('knex')(connectionOptions);
-	console.log('New connection to default postgres database made');
-	console.log(knex);
-	console.log();
-	console.log();
-	console.log();
-	// Remove all other connections to test database
-	await knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`)
-		.catch((err) => {
-			console.log('there was an error!!!!!!!!!!!');
-			console.log(err);
-			return;
-		})
-	console.log('Removed all other connections to the test database');
-
-	// Drop test database if it exists
-	await knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
-	console.log('Dropped test database (if it existed)');
-
-	// Create test database
-	await knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
-	console.log('Test database created');
-};
+const knex = require('knex')(connectionOptions);
+console.log('New connection to default postgres database made');
 
 
-SetUpTestDB()
-	.then(() => process.exit());
+return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
+	.then(() => {
+		console.log('Dropped test database (if it existed)');
+		// Create test database
+		return knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
+	})
+	.then(() => {
+		console.log('Test database created');
+		return process.exit();
+	});
