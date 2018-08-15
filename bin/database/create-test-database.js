9fc56d56a76ad92b7b483aa0caddf14f8295f312
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-15 09:31:39 -0500

    added comments to the create-test-database file

--- bin/database/create-test-database.js
+++ bin/database/create-test-database.js
@@ -5,7 +5,9 @@
 require('dotenv').config();
 
 const connectionOptions = require(`${process.cwd()}/knexfile.js`)[`${process.env.NODE_ENV}`];
+// Change database in connection to default 'postgres' db
 connectionOptions.connection.database = 'postgres';
+
 
 const knex = require('knex')(connectionOptions);
 console.log('New connection to default postgres database made');
@@ -14,10 +16,12 @@ console.log('New connection to default postgres database made');
 // Remove all other connections to test database
 knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`)
 	.then(() => {
+		console.log('Removed all other connections to the test database');
 		// Drop test database if it exists
 		return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
 	})
 	.then(() => {
+		console.log('Dropped test database (if it existed)');
 		// Create test database
 		return knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
 	})
