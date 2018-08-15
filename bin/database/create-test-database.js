c7bd36743cb1689711f13378c066b3eeb2090901
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-15 14:46:19 -0500

    add logs in an attempt to diagnose the problem

--- bin/database/create-test-database.js
+++ bin/database/create-test-database.js
@@ -9,9 +9,19 @@ connectionOptions.connection.database = 'postgres';
 
 const knex = require('knex')(connectionOptions);
 console.log('New connection to default postgres database made');
+console.log();
+console.log();
+console.log(knex.client.config);
+console.log();
+console.log();
 
-
-return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`)
+// Remove all other connections to test database
+knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`)
+	.then(() => {
+		console.log('Removed all other connections to the test database');
+		// Drop test database if it exists
+		return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
+	})
 	.then(() => {
 		console.log('Dropped test database (if it existed)');
 		// Create test database
