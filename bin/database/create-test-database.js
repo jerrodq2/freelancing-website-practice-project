73577bd92b60dc8d0842c88b59eee94a5ce652f1
Jerrod Quintana <jerrodq2@yahoo.com>
2018-08-17 11:49:08 -0500

    testing just creating the database

--- bin/database/create-test-database.js
+++ bin/database/create-test-database.js
@@ -6,19 +6,6 @@ require('dotenv').config();
 
 const connectionOptions = require(`${process.cwd()}/knexfile.js`)[`${process.env.NODE_ENV}`];
 connectionOptions.connection.database = 'postgres';
-connectionOptions.pool.afterCreate = (conn, done) => {
-	console.log('inside');
-	conn.query('SELECT current_database();', (err) => {
-		if (err) {
-			console.log('error? --------------------------------------------------------------------------------------------------------------------------------------------');
-			console.log(err);
-			done();
-		} else {
-			console.log('good !!!!!!!!!!!!!');
-			done();
-		}
-	})
-}
 
 const knex = require('knex')(connectionOptions);
 console.log('New connection to default postgres database made');
@@ -30,18 +17,23 @@ console.log();
 
 // Remove all other connections to test database
 console.log('start');
-knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`)
-	.then(() => {
-		console.log('Removed all other connections to the test database');
-		// Drop test database if it exists
-		return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
-	})
-	.then(() => {
-		console.log('Dropped test database (if it existed)');
-		// Create test database
-		return knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
-	})
+return knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
 	.then(() => {
 		console.log('Test database created');
 		return process.exit();
 	});
+// knex.raw(`select pg_terminate_backend(pid) from pg_stat_activity where datname = '${process.env.PG_TEST_DATABASE}'`)
+// 	.then(() => {
+// 		console.log('Removed all other connections to the test database');
+// 		// Drop test database if it exists
+// 		return knex.raw(`DROP DATABASE IF EXISTS ${process.env.PG_TEST_DATABASE};`);
+// 	})
+// 	.then(() => {
+// 		console.log('Dropped test database (if it existed)');
+// 		// Create test database
+// 		return knex.raw(`CREATE DATABASE ${process.env.PG_TEST_DATABASE};`);
+// 	})
+// 	.then(() => {
+// 		console.log('Test database created');
+// 		return process.exit();
+// 	});
