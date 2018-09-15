9549e189c7b292acf68bd1bfd3a2d41d01ecc44c
Jerrod Quintana <jerrodq2@yahoo.com>
2018-09-15 12:01:58 -0500

    removed commented out code at bottom, put it in a separate word doc

--- src/services/users/routes/clients/test_post.js
+++ src/services/users/routes/clients/test_post.js
@@ -4,7 +4,7 @@
 const { celebrate, Joi } = require('celebrate');
 
 
-// NOTE: this file is a joi example
+// this file is a joi example
 module.exports = (router) => router.post('/post',
 	celebrate({
 		body: Joi.object().keys({
@@ -31,50 +31,12 @@ module.exports = (router) => router.post('/post',
 
 			// a Joi conditional. If the key 'name' (shown above) exists in the body, then the test key is required to be in the body. If the 'name' doesn't exist (isn't in the body), then the test key can be blank (null)
 			test: Joi.string().allow('').when('name', { is: Joi.exist(), then: Joi.string().required(), otherwise: Joi.string().allow('') }),
-		})
+
+			// using .with() at the end of the body keys({}) object requires the presence of other keys when the specificed key is here. Basically, if the 'a' key exists in the body, then the 'b' key is required. If 'a' isn't in the body, 'b' isn't required. Can accept more than one peer, ex: .with('a', ['b', 'c'])
+		}).with('a', 'b')
 		// TODO: should I add info for these below?
 		// .with('a', 'b');
 	}), (req, res) => {
 		return res.status(200).send('Post Successful');
 	}
 );
-
-// NOTES ON Joi - maybe put in a word doc later - https://github.com/hapijs/joi/blob/master/API.md#objectorpeers
-/*
-
-.requied() - requires the key to be included in the req body, it is optional (see below) by default. Specifically, it doesn't allow a key to be undefined
-
-.optional() - allows the key to be undefined, in other words to not be included in the req body. It is basically optional of being in the body. It doesn't affect what value is accepted beyond that. Meaning, if you have "name: Joi.string().optional();" and name is not in the req body, no problem. But if it is in the body and is blank (null), it will error out. Use .allow(value) if you want to allow it to be blank(null) or any other value. You can use both allow and optional together if desired, ex: allow the key to be in the body or not and then allow it to be blank or not. If that isn't the desired behavior, be specific about using optional or allow
-
-.forbidden() - marks a key as forbidden, meaning it will only accept a value of undefined. In other words, if the key exists in the req body, it will error out.
-
-.allow() - allows specific values for the key. For example, if you have "name: Joi.string().allow('');" then name will have to be in the body (look at optional() to allow it to be undefined), but it can also be blank (null). If allow was taken away, it wouldn't be allowed to blank. Multiple values can be specified
-
-.valid() - only allows specific values. For example, if we have "age: Joi.number().required().valid(5, 6)", age will have to be in the body and it can only be 5 or 6, anything else will error out. Also accepts an array of values (ex: valid([5, 'hi', 8]); ).
-
-.default() - sets a value if the key isn't in the body (undefined). If it is in the body, default doesn't affect it. It can also accept a function as a default. Be careful about using it with optional().
-
-.trim() - removes any whitespace in a string before or after the value(ex: "name here   " becomes "name here")
-
-.lowercase() - requries the string to be all lowercase, if the validation "convert" option is on (it is be default), the string will be forced to lowercase.
-
-.uppercase() - same as lowercase() but makes it uppercase.
-
-.string().guid() - requires the string to be in guid format
-
-.string().regex() - requires a string to match the regex given, ex: "Joi.string().regex(/^[abc]+$/);". It also accepts variables, ex: "phone: Joi.string().trim().regex(PHONE_REGEX).optional(),"
-
-.when() - a joi conditional, converts the key to an alternative based on the conditions. for example, if I have
-"letter: Joi.string().when('b', { is: Joi.exist(), then: Joi.valid('y'), otherwise: Joi.valid('z') })"
-then, if the key 'b' exists in the body, it changes the letter key to use .valid and only allow 'y'. But if 'b' doesn't exist in the body, it changes it to only allow 'z' to be valid. A more complicated example, say you have a 'type' key which can be 'dealer' or 'not dealer', and we have:
-"dealerId: Joi.any().forbidden().when('type', { is: 'dealer', then: Joi.string().guid().required() })"
-in this case, the 'dealerId' key is forbidden (can't be in the body) by default. But, if the 'type' key is equal to 'dealer', then it changes the 'dealerId' key be required and be in guid() format
-
-*/
-
-// module.exports = (router) => router.post('/post',
-// 	(req, res) => {
-// 		console.log(req.body);
-// 		return res.status(200).send('Post Successful');
-// 	}
-// );
