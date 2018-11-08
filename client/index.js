fe4fa5d62fd58ab7fcda2ca915ab16151a978912
Jerrod Quintana <jerrodq2@yahoo.com>
2018-11-08 09:40:00 -0600

    trying to get react and webpack to work

--- client/index.js
+++ client/index.js
@@ -15,12 +15,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const e = React.createElement;
 
-const App = () => {
-  return <div>Hello World</div>
+// const App = () => {
+// 	return <div>Hello World</div>
+// };
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { liked: false };
+	}
+
+	render() {
+		if (this.state.liked) {
+			return 'You liked this';
+		}
+
+		return e(
+			'button',
+			{ onClick: () => this.setState({ liked: true }) },
+			'Like'
+		);
+	}
 }
+// ReactDOM.render(
+// 	<App />,
+// 	document.querySelector('#app')
+// );
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#app')
-);
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(App), domContainer);
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,15 @@
 
 
 // import webpack from 'webpack';
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const LiveReloadPlugin = require('webpack-livereload-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'),
+	// LiveReloadPlugin = require('webpack-livereload-plugin'),
+	HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+		template: __dirname + '/client/index.html',
+		filename: 'index.html',
+		inject: 'body',
+	});
 
-export default  {
+module.exports =  {
 	mode: 'development',
 	entry: __dirname + '/client/index.js',
 	module: {
@@ -24,26 +29,22 @@ export default  {
 	},
 
 	output: {
+		filename: 'index.js',
 		path: __dirname + '/build',
-		filename: 'transformed.js'
 	},
 
 	plugins: [
-		new HtmlWebpackPlugin({
-			template: __dirname + '/client/index.html',
-			filename: 'index.html',
-			inject: 'body',
-		}),
-		new LiveReloadPlugin(),
+		HtmlWebpackPluginConfig,
+		// new LiveReloadPlugin(),
 	]
 };
 
 
-// // this allows webpack to create a new html file to go with the JS file created below, so they will be in the same directory
+// this allows webpack to create a new html file to go with the JS file created below, so they will be in the same directory
 // const HTMLWebpackPlugin = require('html-webpack-plugin'),
 // 	HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 // 		// template is where the current html file is located
-// 		template: __dirname + '/public/index.html',
+// 		template: __dirname + '/client/index.html',
 // 		// this is the name of the newly created html file
 // 		filename: 'index.html',
 // 		// webpack injects a <script> tag in the new html file, linking it to the new JS file. This tells it to either inject it in the 'body' or 'head' of the new html file
@@ -54,7 +55,7 @@ export default  {
 // module.exports = {
 // 	mode: 'development',
 // 	// tells webpack what the entry point is, or which file it will transform
-// 	entry: __dirname + '/public/index.js',
+// 	entry: __dirname + '/client/index.js',
 // 	// the loaders array below details each transformation your code will go through before it reaches the browser. Each loader is written as an object
 // 	module: {
 // 		rules: [
