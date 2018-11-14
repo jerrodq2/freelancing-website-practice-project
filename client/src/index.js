7cebc35d2b8803edf9e26bf611a3cbca0283b50e
Jerrod Quintana <jerrodq2@yahoo.com>
2018-11-14 15:24:26 -0600

    a few small changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,60 @@
+'use strict';
+//
+//
+// const React = require('react'),
+// 	ReactDOM = require('react-dom'),
+// 	App = require('./components/App');
+//
+//
+// ReactDOM.render(
+// 	<App />,
+// 	document.getElementById('app')
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../resources/scss/style.scss';
+const e = React.createElement;
 
+// const App = () => {
+// 	return <div>Hello World</div>
+// };
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { liked: false };
+	}
+
+	render() {
+		if (this.state.liked) {
+			return 'You liked this';
+		}
+		return (
+			<button onClick={() => this.setState({ liked: true })}>
+				'Like'
+			</button>
+		);
+		// return e(
+		// 	'button',
+		// 	{ onClick: () => this.setState({ liked: true }) },
+		// 	'Like'
+		// );
+	}
+}
+// ReactDOM.render(
+// 	<App />,
+// 	document.querySelector('#app')
+// );
+
+// const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(
-  <p> Hello World </p>,
-  document.getElementById('root'),
+	<App />,
+	// document.getElementById('#like_button_container')
+	document.querySelector('#like_button_container')
 );
+
+
+//
+//
