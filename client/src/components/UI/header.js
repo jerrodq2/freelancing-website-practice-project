b8dd84e37f9a4fdf4c3b82789b786b3b3e0b66a0
Jerrod Quintana <jerrodq2@yahoo.com>
2018-12-12 15:09:21 -0600

    made changes to react code, took out starting files that were no longer used, and added a starting header

---
+++ client/src/components/UI/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+	render() {
+		return (
+		  <nav className="navbar fixed-top navbar-dark  bg-dark ">
+				<div className="container">
+					<h1 className="navbar-brand ">Navbar</h1>
+				</div>
+		  </nav>
+		)
+	}
+};
+
+
+export default Header;
